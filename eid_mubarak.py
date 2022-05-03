3e66cf9725c815b39ba84939549ccb762031122e
SpiderX <abirali20205@gmail.com>
2022-05-03 19:20:12 +0600

    bug fixed !

--- eid_mubarak.py
+++ eid_mubarak.py
@@ -3,7 +3,6 @@ import random
 from turtle import *
 
 # Screen
-
 Screen()
 title("Eid Mubarak !")
 window_width()
@@ -15,7 +14,6 @@ speed(0)
 
 def Star_turtle(x, y):
     # Star
-    import random
     penup()
     goto(x, y)
     colour = ['blue', 'red', 'white', 'green', 'gold', 'maroon', 'violet', 'magenta', 'purple', 'navy',
@@ -38,6 +36,7 @@ def Star_turtle(x, y):
 
 
 def Moon_turtle():
+    setheading(0)
     # Moon
     up()
     bgcolor('black')
@@ -59,17 +58,19 @@ def Moon_turtle():
 def Write_turtle():
     color('white')
     penup()
-    goto(-96, 41)
+    goto(-130, 85)
     pendown()
     write("Eid Mubarak", font=("Courier", 24, "normal"))
 
 
 if __name__ == "__main__":
-    for i in range(15):
+    n_of_stars = 50
+    for i in range(n_of_stars):
         x = random.randrange(-250, 250, 20)
         y = random.randrange(-250, 250, 15)
         j = random.randrange(0, 18)
         Star_turtle(x, y)
+
     Moon_turtle()
     Write_turtle()
 
